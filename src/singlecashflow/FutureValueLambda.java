5acd2ba31b601d55e849dae2be2478b6c3c3a8b5
Benet Boscòri <benet.datascience@gmail.com>
2023-06-10 13:47:20 +0700

    Benet transform data types of presentValue and futureValue variables into BigDecimal

--- src/singlecashflow/FutureValueLambda.java
+++ src/singlecashflow/FutureValueLambda.java
@@ -1,5 +1,6 @@
 package singlecashflow;
 
+import java.math.BigDecimal;
 import java.util.Scanner;
 import java.util.function.Function;
 
@@ -9,21 +10,23 @@ public class FutureValueLambda {
 
         // Enter principal
         System.out.print("Principal: ");
-        double presentValue = sc.nextDouble();
+        BigDecimal presentValue = new BigDecimal(sc.nextDouble());
 
         // Enter interest rate
         System.out.print("Interest rate: ");
         double interestRate = sc.nextDouble();
 
-        //Enter number of periods
+        // Enter number of periods
         System.out.print("Number of periods: ");
         int periods = sc.nextInt();
 
-        // Future value formula by lambda expression
-        Function<Integer, Double> futureValue = n -> {
-            return presentValue * Math.pow(1 + interestRate/100, n);
+        // Using lambda expressions to calculate future value
+        Function<Integer, BigDecimal> futureValue = n-> {
+            return presentValue.multiply(BigDecimal.valueOf(Math.pow(1+interestRate/100, n)));
         };
-        System.out.printf("You received after %d year(s) with %.2f: %.4f",
+
+        // Print the result
+        System.out.printf("You received after %d year(s) with annual interest rate of %.2f%%: %.4f",
                           periods, interestRate, futureValue.apply(periods));
     }
 }
